9a3fdcf6cda15e0758be298fc62228958e11d911
SurjeshYadavMasai <surjeshom@gmail.com>
2022-05-21 22:56:33 +0530

    UI

---
+++ src/components/UserInfo.js
@@ -0,0 +1,38 @@
+export const userInfo = [
+  {
+    id: 1,
+    imgUrl: "",
+    name: "Surjesh Yadav",
+    email: "surjesh@gmail.com",
+  },
+  {
+    id: 2,
+    imgUrl: "",
+    name: "Rahul Gaur",
+    email: "rahul@gmail.com",
+  },
+  {
+    id: 3,
+    imgUrl: "",
+    name: "Vikash Saxena",
+    email: "vikash@gmail.com",
+  },
+  {
+    id: 4,
+    imgUrl: "",
+    name: "Abdul Khaliq",
+    email: "abdul@gmail.com",
+  },
+  {
+    id: 5,
+    imgUrl: "",
+    name: "Arvind Kumar",
+    email: "arving@gmail.com",
+  },
+  {
+    id: 6,
+    imgUrl: "",
+    name: "Aman Gupta",
+    email: "aman@gmail.com",
+  },
+];
